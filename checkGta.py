8ea156aa78f8689cf0df2312c1e08934dc342db4
Nuadi Bantine <nuadi@users.noreply.github.com>
2015-04-15 18:14:13 -0400

    Update checkGta.py
    Expanded ignore list, cleaned up whitespace, added comments

--- checkGta.py
+++ checkGta.py
@@ -1,33 +1,54 @@
 import hashlib
 import os
 
-ignoreFiles = ['Grand Theft Auto V\commandline.txt',
-               'Grand Theft Auto V\GTA5.exe',
-               'Grand Theft Auto V\GTAVLauncher.exe',
-               'Grand Theft Auto V\PlayGTAV.exe']
+gtaDirectory = 'Grand Theft Auto V'
 
+# Initialize a list of files to ignore based on the install directory
+ignoreFiles = ['commandline.txt',
+               'GTA5.exe',
+               'GTAVLauncher.exe',
+               'PlayGTAV.exe',
+               'ReadMe\Chinese\ReadMe.txt',
+               'ReadMe\English\ReadMe.txt',
+               'ReadMe\French\ReadMe.txt',
+               'ReadMe\German\ReadMe.txt',
+               'ReadMe\Italian\ReadMe.txt',
+               'ReadMe\Japanese\ReadMe.txt',
+               'ReadMe\Korean\ReadMe.txt',
+               'ReadMe\Mexican\Readme.txt',
+               'ReadMe\Polish\ReadMe.txt',
+               'ReadMe\Portuguese\ReadMe.txt',
+               'ReadMe\Russian\ReadMe.txt',
+               'ReadMe\Spanish\ReadMe.txt']
+ignoreList = []
+for ignoreFile in ignoreFiles:
+  ignoreList.append(os.path.join(gtaDirectory, ignoreFile))
+
+# Initialize the log file, or clear it if it's present
 with open('checkGta.log', 'w') as log:
   log.write('')
 
-gtaDirectory = 'Grand Theft Auto V'
-
+# Ingest the master hash list
 hashList = {}
 with open('hashes.txt', 'r') as hashFile:
   lineType = 0
   fileName = ''
   for line in hashFile:
+    # Find the new line, if present
     newLineIndex = line.find('\n')
     if lineType == 0:
+      # This line should be the file name, including subdirectories
       if newLineIndex > -1:
         fileName = os.path.join(gtaDirectory, line[:newLineIndex])
       else:
         fileName = os.path.join(gtaDirectory, line)
       lineType += 1
-      #print fileName
+      #print fileName #diagnostic
     elif lineType == 1:
-      # Skip this line
+      # Skip this line, only used for notes
       lineType += 1
     elif lineType == 2:
+      # This line should be the file hash
       fileHash = -1
       if newLineIndex > -1:
         fileHash = line[:newLineIndex]
@@ -36,10 +57,12 @@ with open('hashes.txt', 'r') as hashFile:
       hashList[fileName] = fileHash
       lineType = 0
 
+# Setup some buckets for counting
 okayFiles = 0
 badFiles = 0
 unknownFiles = 0
 
+# Walk through all files in the install directory
 for dirpath, dirnames, filenames in os.walk(gtaDirectory):
   for filename in filenames:
     gtaFile = os.path.join(dirpath, filename)
@@ -54,41 +77,39 @@ for dirpath, dirnames, filenames in os.walk(gtaDirectory):
           hasher.update(buf)
           buf = afile.read(BLOCKSIZE)
       gtaHash = hasher.hexdigest()
-      #print('%s %s' % (gtaFile, gtaHash))
+      #print('%s %s' % (gtaFile, gtaHash)) #diagnostics
 
       # Pull the hash for this file
-      if gtaFile in hashList:
-        fileHash = hashList[gtaFile]
-        if fileHash == gtaHash:
-          status = '%s OK!' % gtaFile
+      fileHash = hashList[gtaFile]
+      if fileHash == gtaHash:
 
-          with open('checkGta.log', 'a') as log:
-            log.write(status + '\n')
-          print status
+        status = '%s OK!' % gtaFile
+        with open('checkGta.log', 'a') as log:
+          log.write(status + '\n')
+        print status
+        okayFiles += 1
 
-          okayFiles += 1
-        else:
-          status = '%s CORRUPT!' % gtaFile
-          expected = 'Expected \'%s\' but found \'%s\'' % (fileHash, gtaHash)
+      else:
+        status = '%s CORRUPT!' % gtaFile
+        expected = 'Expected \'%s\' but found \'%s\'' % (fileHash, gtaHash)
+        with open('checkGta.log', 'a') as log:
+          log.write(status + '\n')
+          log.write(expected + '\n')
+        print status
+        print expected
+        badFiles += 1
 
-          with open('checkGta.log', 'a') as log:
-            log.write(status + '\n')
-            log.write(expected + '\n')
-          print status
-          print expected
-
-          badFiles += 1
-    elif gtaFile not in ignoreFiles and gtaFile.find('.part') == -1 and gtaFile.find('.hash') == -1:
-
+    elif gtaFile not in ignoreList and gtaFile.find('.part') == -1 and gtaFile.find('.hash') == -1:
+      # Not sure about this file, output for inspection
       status = 'Unknown file: %s' % gtaFile
-
       with open('checkGta.log', 'a') as log:
         log.write(status + '\n')
       print status
-
       unknownFiles += 1
 
+# All files processed, output results
 print '%s files OK, %s files CORRUPT, %s files unknown' % (okayFiles, badFiles, unknownFiles)
 
+# Pause for the folks that double-click
 enter = raw_input('Press ENTER to complete the script...')
 print 'Script complete.'
